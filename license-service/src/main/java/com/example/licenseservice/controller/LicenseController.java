60fbf021bede791cfbd9e5990b9071ae87ff62a1
deyuan.qin <deyuanqin@163.com>
2018-09-14 18:17:17 +0800

    保存

---
+++ license-service/src/main/java/com/example/licenseservice/controller/LicenseController.java
@@ -0,0 +1,18 @@
+package com.example.licenseservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class LicenseController {
+
+    @Value("${test.testString}")
+    private  String testString;
+
+    public String index() {
+        return "license index: " + this.testString;
+    }
+
+}
